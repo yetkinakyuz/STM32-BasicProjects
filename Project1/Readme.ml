58e5934a3d0759ef395507af54c12d27035228e8
Yetkin AKYÜZ <contact@yetkinakyuz.com>
2021-04-02 10:09:15 +0300

    update readme

--- Project1/Readme.ml
+++ Project1/Readme.ml
@@ -2,3 +2,5 @@ There are two buttons and two leds. Also each button contols only one led.
 
 One of the buttons is connected to PC2 as pull-up and the other is connected to PC8 as pull-down.
 One of the leds is connected to PA12 as open-drain and the other led is connected to PA11 as push-pull.
+
+Project Video: https://youtu.be/9CNwEpNsLxk

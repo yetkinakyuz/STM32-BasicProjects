9f5db54c24c1495cc6c84d2011a78c99de2b22bf
Yetkin AKYÜZ <yetkin.akyz@gmail.com>
2021-04-21 14:26:02 +0300

    Add files via upload

---
+++ Project3/LCD_ScrollingText/Core/Src/main.c
@@ -0,0 +1,329 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "string.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+char* msg1 = "TEXT  IS";
+char* msg2 = "SCROLLING";
+char* empty = "                ";
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+void InitLCD(void);
+void lcd_init_write(unsigned char value);
+void epulse(void);
+void delay_us ();
+void lcd_cmd(unsigned char cmd);
+void lcd_write_data (char harf);
+void lcd_data(unsigned char satir, unsigned char sutun, const char *mesaj);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+  InitLCD ();
+
+  lcd_data(1, 5, msg1);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	for (int i = 1; i <= 17 - strlen(msg2); i++)
+	{
+		lcd_data(2, i-1, " ");
+		lcd_data(2, i, msg2);
+
+		HAL_Delay(500);
+	}
+
+	for (int j = 17 - strlen(msg2); j >= 1; j--)
+	{
+		lcd_data(2, j + strlen(msg2), " ");
+		lcd_data(2, j, msg2);
+
+		HAL_Delay(500);
+	}
+
+	/* USER CODE END WHILE */
+  }
+  /* USER CODE BEGIN 3 */
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Enables the Clock Security System
+  */
+  HAL_RCC_EnableCSS();
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LCD_RS_Pin|LCD_E_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, D4_Pin|D5_Pin|D6_Pin|D7_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : LCD_RS_Pin LCD_E_Pin D4_Pin D5_Pin
+                           D6_Pin D7_Pin */
+  GPIO_InitStruct.Pin = LCD_RS_Pin|LCD_E_Pin|D4_Pin|D5_Pin
+                          |D6_Pin|D7_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+void InitLCD (void)
+{
+  HAL_Delay (200);
+  lcd_init_write(0x30); //Special Sequence:Write Function Set.
+  HAL_Delay (10);
+  lcd_init_write(0x30); //Special Sequence:Write Function Set.
+  HAL_Delay (10);
+  lcd_init_write(0x30); //Special Sequence:Write Function Set.
+  HAL_Delay (10);
+  lcd_init_write(0x20); //Special Sequence:Write Function Set.
+  HAL_Delay (10);
+
+  lcd_cmd(0x28);//Function set(001(DL)NFxx)-DL=0 (4bit), N=1 (2 satir),F=0 (5X8))
+  lcd_cmd(0x08);//Display control (0000 1DCB)-display is off, cursor is off, blinking is off
+  lcd_cmd(0x01);//Clear Display
+  lcd_cmd(0x06);  // Entry mode set (0000 01(I/D)S), Automatic Increment - No Display shift.
+   // Initialization ends
+  lcd_cmd(0x0C);//Display control(0000 1DCB)--display is on, cursor is off, blinking is off
+
+}
+void lcd_init_write(unsigned char value)
+{
+
+	HAL_GPIO_WritePin (LCD_RS_GPIO_Port, LCD_RS_Pin,0);  //LCD_RS=0;
+	GPIOB->ODR= (GPIOB->ODR & 0x0F) | (value & 0xF0) ;        //Make No Affect on other Port Pins
+	epulse(); //Send Enable Signal to LCD
+	HAL_GPIO_WritePin (LCD_RS_GPIO_Port, LCD_RS_Pin,1);  //LCD_RS=1;
+
+}
+void epulse()
+{
+	HAL_GPIO_WritePin (LCD_E_GPIO_Port, LCD_E_Pin,1);  //LCD_E=1;
+	delay_us();
+	HAL_GPIO_WritePin (LCD_E_GPIO_Port, LCD_E_Pin,0);  //LCD_E=0;
+	delay_us();
+}
+void delay_us ()
+{
+	unsigned int time=680; //delay=1us/170x2x680=8us
+	while (time--);
+
+}
+void lcd_cmd(unsigned char cmd)
+{
+	     HAL_GPIO_WritePin (LCD_RS_GPIO_Port, LCD_RS_Pin,0);  //LCD_RS=0;
+	     GPIOB->ODR= (GPIOB->ODR & 0x0F) | (cmd & 0xF0) ;        //Make No Affect on other Port Pins
+	     //Send Higher Nibble to LCDPORT
+	     epulse(); //Send Enable Signal to LCD
+	     GPIOB->ODR = (GPIOB->ODR & 0x0f) | (cmd<<4);//Make No Affect on other Port Pins
+	     //Send Lower Nibble to LCDPORT
+	     epulse(); //Send Enable Signal to LCD
+	     HAL_GPIO_WritePin (LCD_RS_GPIO_Port, LCD_RS_Pin,1);  //LCD_RS=1;
+	     HAL_Delay(10);
+
+
+}
+void lcd_write_data (char harf)
+{
+	HAL_GPIO_WritePin (LCD_RS_GPIO_Port, LCD_RS_Pin,1);  //LCD_RS=1;
+	GPIOB->ODR = (GPIOB->ODR & 0x0f) | (harf & 0xf0);//Make No Affect on other Port Pins
+     //Send Higher Nibble to LCDPORT
+    epulse(); //Send Enable Signal to LCD
+    GPIOB->ODR = (GPIOB->ODR & 0x0f) | (harf<<4);//Make No Affect on other Port Pins
+     //Send Lower Nibble to LCDPORT
+     epulse(); //Send Enable Signal to LCD
+     HAL_GPIO_WritePin (LCD_RS_GPIO_Port, LCD_RS_Pin,0);  //LCD_RS=0;
+
+     HAL_Delay(10);
+
+
+}
+void lcd_data(unsigned char satir, unsigned char sutun, const char *mesaj)
+{
+	char temp;
+
+	switch(satir)
+	{
+		case 1:
+			temp=sutun-1+ 0x80;
+			break;
+		case 2:
+			temp= sutun-1+0xc0;
+			break;
+	}
+
+	lcd_cmd(temp);
+
+	do
+	{
+		lcd_write_data(*mesaj++);
+
+	}while(*mesaj);
+
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
